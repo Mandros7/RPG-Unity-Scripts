fe5d55fc4dc62b9f8746cb8890453471bacf2444
Mandros7 <hectorrogue.9@gmail.com>
2015-02-18 21:05:53 +0100

    Uploaded scripts
    Both with Spanish comments (so rest of the work group understands them
    better).

---
+++ PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+Este script se encarga del movimiento del jugador y de la rotacion natural al cambiar de dirección
+Tiene que aplicarse a un objeto que tenga un componente RigidBody. 
+*/
+
+public class PlayerMovement : MonoBehaviour
+{
+	// Declaracion de variables publicas (para modificarlas en el testeo)
+	public float turnSmoothing = 15f;   // Un valor que indica como de suave sera el giro del jugador
+	public float speed = 5.0f;
+	
+	void FixedUpdate ()
+	{
+		// Obtenemos las entradas y las almacenamos en variables
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+		MovementManagement(h, v);
+	}
+	
+	
+	void MovementManagement (float horizontal, float vertical)
+	{
+		// Primero comprobamos que hay inputs.
+		if(horizontal != 0f || vertical != 0f)
+		{
+			// Le pasamos los valores a la funcion de rotacion
+			Rotating(horizontal, vertical);
+
+			//Comprobamos si estamos moviendonos en diagonal. Pulsando mas de una tecla.
+			if(horizontal != 0f && vertical != 0f){
+				/* La combinar las coordenadas horizontal y vertical de esta forma (la componente Y es altura)
+					Se consigue cambiar la orientación del movimiento, de forma que el personaje ahora se mueve 
+					hacia arriba al pulsar la tecla Up o W.
+				*/
+				Vector3 targetDirection = new Vector3(horizontal+vertical, 0f, vertical-horizontal);
+				rigidbody.MovePosition(rigidbody.position + targetDirection * speed * Time.deltaTime);
+			}
+			else {
+				// Para movimientos con una sola componente
+				Vector3 targetDirection = new Vector3(horizontal+vertical, 0f, vertical-horizontal);
+				if (vertical!=0f){
+					// En el caso de movimiento vertical [arriba y abajo], es necesario ajustar la velocidad (x1.5)
+					rigidbody.MovePosition(rigidbody.position + targetDirection * speed * Time.deltaTime * 1.5f);
+				}
+				else {
+					rigidbody.MovePosition(rigidbody.position + targetDirection * speed * Time.deltaTime);
+				}
+			}
+		}
+	}
+	
+	
+	void Rotating (float horizontal, float vertical)
+	{
+		// Nuevo vector con la combinación de componentes (la suma y resta cambian la orientacion)
+		Vector3 targetDirection = new Vector3(horizontal+vertical, 0f, vertical-horizontal);
+	
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		
+		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		
+		rigidbody.MoveRotation(newRotation);
+	}
+
+}
---
+++ SmoothFollow2.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Versión 2 del movimiento de la cámara. Se define un rango fuera del cual la camara seguirá al target
+
+public class SmoothFollow2 : MonoBehaviour {
+
+
+
+	public Transform target; //Objetivo. En este caso el jugador
+	public float distance;	
+	//public float height = 3.0f;
+	public float damping = 5.0f;
+	public float rotationDamping = 10.0f;
+	private Vector3 offset; // Mide la posicion inicial relativa de ambos objetos
+	private float current_distance; // Distancia entre ambos objetos
+	public float range; // Rango máximo de distancia permitida sin mover la camara
+	private Vector3 old_wantedPosition;
+	public float fixed_Horizontal = 2.0f; //Variable de ajuste del seguimiento horizontal
+
+	void Start () {
+		// Inicialmente se asignan valores a las variables.
+		offset = transform.position - target.transform.position;
+		distance = Vector3.Distance (transform.position, target.transform.position);
+		old_wantedPosition = transform.position;
+	}
+
+	bool checkRange(){
+		//Funcion que comprueba si se ha salido del rango
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		current_distance = Vector3.Distance (transform.position, target.transform.position);
+		if (v==0 && h!=0){
+			 //Se ajusta el valor cuando el movimiento es solo horizontal
+			current_distance*=fixed_Horizontal;
+		}
+		if (Mathf.Abs (current_distance-distance)>range){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void FixedUpdate () {
+		Vector3 wantedPosition;
+		// Si estamos fuera del rango actualizamos la posicion deseada de la camara.
+		if (checkRange ()) {
+						wantedPosition = new Vector3 (target.transform.position.x + offset.x, target.transform.position.y + offset.y, target.transform.position.z + offset.z);
+						old_wantedPosition = wantedPosition;
+				}
+		// Movemos la camara.
+		transform.position = Vector3.Slerp (transform.position, old_wantedPosition, Time.deltaTime * damping);
+	}
+}
